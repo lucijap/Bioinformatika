a65a35afae407430f87729220b7b81e1635cc1e4
Mestro <marko.mestrovic@outlook.com>
2020-01-13 15:32:42 +0100

    Cleaning up blast.cpp

--- src/blast.cpp
+++ src/blast.cpp
@@ -2,7 +2,6 @@
 #include <string>
 #include <map>
 #include <list>
-#include <iostream>
 #include <vector>
 #include <algorithm>
 
@@ -27,34 +26,9 @@ void printAllKLengthRec(char set[], string prefix,list<string>*result ,int n, in
 
 }
 
-list<string> getAllKLength(char set[], int k,int n){
-    list<string> result;
-    printAllKLengthRec(set, "", &result,n, k);
-    return result;
-}
+
 
 vector<string> Blast(vector<string>* reference_kmers, map<string, list<int>>* seq_kmers,int k){
-    /*
-    int thresh = 7;
-    char bases [4]= {'A','C','G','T'};
-
-    for(unsigned int i=0;i<sequence.size()-k;i++){
-        string k_mer=sequence.substr(i,(unsigned int)k);
-
-        for(auto const&k_mer1:all_kmers){
-            int sum = 0;
-            for(unsigned int j=0;j<k;j++){
-                if (k_mer.at(j) == k_mer1.at(j)) {
-                    sum += 1;
-                }
-            }
-            if (sum>=thresh){
-                kmers_to_compare.push_back(k_mer1);
-            }
-        }
-    }
-     */
-
 
     // generate list of keys in reference_kmers
 
