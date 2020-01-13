c8d952dff42a03145adbcb573322c2e2a0da14f8
Lucija Planinić <lucijaplaninic@gmail.com>
2020-01-13 15:29:10 +0100

    Minor changes in Smith Waterman

--- src/smith_waterman.cpp
+++ src/smith_waterman.cpp
@@ -40,12 +40,14 @@ pair<string, string> SmithWaterman(string sequence_A, string sequence_B, int reg
     for (int i = 1; i < dim_A + 1; i++) {
         current_line[0] = 0;
         for (int j = 1; j < dim_B + 1; j++) {
+            // detect max transformation
             match = last_line[j - 1] + SimilarityScore(sequence_A[i - 1], sequence_B[j - 1]);
             insertion = current_line[j - 1] - penalty;
             deletion = last_line[j] - penalty;
             maximum_value = max({0, match, insertion, deletion});
             current_line[j] = (short) maximum_value;
             if (maximum_value != 0) {
+                // indices for traceback
                 if (maximum_value == match) {
                     traceback.insert({pair<int, int>(i, j), pair<int, int>(i - 1, j - 1)});
                 } else if (maximum_value == insertion) {
@@ -63,7 +65,7 @@ pair<string, string> SmithWaterman(string sequence_A, string sequence_B, int reg
             last_line[k] = current_line[k];
         }
     }
-    ofstream outfile ("ecoli_mutated_test.csv");
+    ofstream outfile ("../train-data/ecoli_mutated_test.csv");
     string first;
     string second;
     list<tuple<char,int,char>> list_of_mutations;
@@ -80,63 +82,34 @@ pair<string, string> SmithWaterman(string sequence_A, string sequence_B, int reg
                 char_of_second=sequence_B[next_pair.second];
                 mutation_tuple = make_tuple(mutation_char,sum, char_of_second);
                 list_of_mutations.push_back(mutation_tuple);
-                //cout << next_pair.first + region_start << endl; //mutacija mijenjanje
             }
-            //first += sequence_A[next_pair.first];
-            //second += sequence_B[next_pair.second];
+
         } else if (next_pair.first == max_pair.first - 1 and next_pair.second == max_pair.second) {
-            //first += sequence_A[next_pair.first];
-            //second += "-";
-            //cout << next_pair.first + region_start << endl;// umetanje
+
             mutation_char ='D';
             sum=next_pair.first+region_start;
             char_of_second=sequence_A[next_pair.first];
             mutation_tuple = make_tuple(mutation_char,sum, char_of_second);
             list_of_mutations.push_back(mutation_tuple);
         } else if (next_pair.first == max_pair.first and next_pair.second == max_pair.second - 1) {
-            //first += "-";
-            //second += sequence_B[next_pair.second];
+
             mutation_char ='I';
             sum=next_pair.first+region_start;
             char_of_second=sequence_B[next_pair.second];
             mutation_tuple = make_tuple(mutation_char,sum, char_of_second);
             list_of_mutations.push_back(mutation_tuple);
-            //cout << next_pair.first + region_start << endl;//brisanje
         }
         max_pair = next_pair;
     }
 
     list_of_mutations.reverse();
-//    sort(list_of_mutations.begin(), list_of_mutations.end(), sortbysec);
     for(auto const&mutation:list_of_mutations){
         outfile << get<0>(mutation) << "," << get<1>(mutation) << "," <<get<2>(mutation)<<endl;
     }
     reverse(first.begin(), first.end());
     reverse(second.begin(), second.end());
     outfile.close();
-    /*
-    cout << first << endl;
-    cout << second << endl;
 
-    cout << "\t\t";
-    for (int i = 0; i < dim_B; i++) {
-        cout << sequence_B[i] << "\t";
-    }
-
-    cout << endl;
-
-    for (int i = 0; i < dim_A + 1; i++) {
-        if (i > 0) {
-            cout << sequence_A[i - 1] << "\t";
-        } else {
-            cout << "\t";
-        }
-        for (int j = 0; j < dim_B + 1; j++) {
-            cout << scoring_matrix[i][j] << "\t";
-        }
-        cout << endl;
-    }
-    */
     return pair<string, string> (first, second);
 
 }
